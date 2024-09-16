03f5a913fdd58fca99ef07c3a77c2c90b45e914f
Mariana <mariana.montes@kuleuven.be>
2024-09-16 17:08:17 +0200

    Add docstrings to avu2json module
    See issue #1

--- src/avu2json/avu2json.py
+++ src/avu2json/avu2json.py
@@ -4,6 +4,19 @@ import argparse
 
 
 def parse_mango_metadata(schema_path, data_object, schema_prefix="mgs"):
+    """Parse AVUs from ManGO metadata schema.
+
+    Args:
+        schema_path (str): Path to a JSON of a ManGO schema.
+        data_object (irods.DataObject): Data object to extract the metadata from.
+        schema_prefix (str, optional): Prefix used in the namespacing of the schema metadata. Defaults to "mgs".
+
+    Raises:
+        FileNotFoundError: If the schema is not found in the path provided.
+
+    Returns:
+        dict: Parsed and validated metadata in (nested) dictionary format without namespacing.
+    """
     if not os.path.exists(schema_path):
         raise FileNotFoundError
     schema = read_schema(schema_path, schema_prefix)
@@ -11,17 +24,51 @@ def parse_mango_metadata(schema_path, data_object, schema_prefix="mgs"):
 
 
 def read_schema(schema_path, schema_prefix="mgs"):
+    """Read schema from file
+
+    This is an isolated function just so the `mango_mdschema` package is only
+    imported when needed and it can be used both for reading metadata from an
+    arbitrary dictionary and for extracting from an iRODS data object.
+
+    Args:
+        schema_path (str): Path to a JSON of a ManGO schema.
+        schema_prefix (str, optional): Prefix used in the namespacing of the schema metadata. Defaults to "mgs".
+
+    Returns:
+        mango_mdschema.Schema: Representation of the schema, for validation and extraction of metadata.
+    """
     from mango_mdschema import Schema
 
     return Schema(schema_path, prefix=schema_prefix)
 
 
-def parse_json_metadata(schema_path, dictionary, schema_prefix="mgs"):
-    schema = read_schema(schema_path, schema_prefix)
+def parse_json_metadata(schema_path, dictionary):
+    """Parse metadata from a dictionary
+
+    Args:
+        schema_path (str): Path to a JSON of a ManGO schema.
+        dictionary (dict): Metadata without namespacing but matching a schema specification.
+
+    Returns:
+        dict: Validated metadata
+    """
+    schema = read_schema(schema_path)
     return schema.validate(dictionary)
 
 
 def extract_template(path):
+    """Read Dataverse template from path
+
+    Args:
+        path (str): Path to the JSON of the Dataverse template whose values need to be filled in
+          with actual metadata.
+
+    Raises:
+        FileNotFoundError: When the path to the template is not found.
+
+    Returns:
+        dict: Contents of the template, to be filled in with actual metadata.
+    """
     if not os.path.exists(path):
         raise FileNotFoundError
     with open(path) as f:
@@ -30,16 +77,44 @@ def extract_template(path):
 
 
 def fill_in_template(template, avus):
+    """Fill in Dataverse template with metadata
+
+    This function does not return anything but just modifies the template itself (the dict).
+
+    Args:
+        template (dict): Contents of the template, e.g. output of `extract_template()`.`
+        avus (dict): Dictionary with metadata, with keys matching the required fields in the template.
+    """
     fields = template["datasetVersion"]["metadataBlocks"]["citation"]["fields"]
     new_fields = [update_template(field, avus) for field in fields]
     template["datasetVersion"]["metadataBlocks"]["citation"]["fields"] = new_fields
 
 
 def return_dict(value, fromAvu):
+    """Make `update_template()` recursive
+
+    Args:
+        value (dict): Contents of the "value" attribute of a field in the Dataverse template.
+        fromAvu (dict): Key-value pairs to fill in this subset of the template
+
+    Returns:
+        dict: Key-value pairs with the matching between a nested field and its corresponding AVUs.
+    """
     return {k: update_template(value[k], fromAvu) for k in value.keys()}
 
 
 def update_template(field, avus_as_json):
+    """Match a template field to the corresponding metadata
+
+    `return_dict()` is used to apply `update_template()` to nested subsets.
+
+    Args:
+        field (dict): Field in the Dataverse template (part of the "fields" array).
+        avus_as_json (dict): Key-value pairs with metadata, maybe nested
+
+    Returns:
+        dict: Filled in field to update the template with.
+    """
     typeName = field["typeName"]
     value = field["value"]
     fromAvu = avus_as_json[typeName]
@@ -56,6 +131,9 @@ def update_template(field, avus_as_json):
 
 
 if __name__ == "__main__":
+    """This is only for testing and demo'ing. The idea is that this is called as a module and the functions used as part
+    of some automated script.
+    """
     parser = argparse.ArgumentParser(
         description="Parse iRODS Metadata for Dataverse submission."
     )
