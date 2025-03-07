05a8b074f6ed52741a6df249d9417ca8d13b8c74
kafetzakid <danai.kafetzaki@kuleuven.be>
2025-03-07 17:46:48 +0100

    formatting, spellings, imports

--- src/irods2dataverse/cli_input.py
+++ src/irods2dataverse/cli_input.py
@@ -1,62 +1,54 @@
-from .metadatablocks import Metadatablocks
 import json
-from rich.prompt import Prompt, Confirm
+from rich.prompt import Prompt
 from pathlib import Path
 import re
 from datetime import datetime
 
 
-
-
 # Reads contents with UTF-8 encoding and returns str.
+
 
 def get_controlled_vocabulary(name):
 
     controlled_vocabularies = {
-                "subject": {
-                    "values": [
-                        "Agricultural Sciences",
-                        "Arts and Humanities",
-                        "Astronomy and Astrophysics",
-                        "Business and Management",
-                        "Chemistry",
-                        "Computer and Information Science",
-                        "Earth and Environmental Sciences",
-                        "Engineering",
-                        "Law",
-                        "Mathematical Sciences",
-                        "Medicine, Health and Life Sciences",
-                        "Physics",
-                        "Social Sciences",
-                        "Other",
-                        "Demo Only"
-                    ],
-                    "description": "Controlled list of subjects for DEMO Dataverse"
-                },
-                "accessRights": {
-                    "values": [
-                        "open",
-                        "restricted",
-                        "embargoed",
-                        "closed"
-                    ],
-                    "description": "Controlled list of acces rights for RDR"
-                },
-                "legitimateOptout": {
-                    "values": [
-                        "privacy",
-                        "intellectual property rights",
-                        "ethical aspects",
-                        "aspects of dual use",
-                        "other"
-                    ],
-                    "description": "Controlled list of legitimate opt out for RDR"
-                }
+        "subject": {
+            "values": [
+                "Agricultural Sciences",
+                "Arts and Humanities",
+                "Astronomy and Astrophysics",
+                "Business and Management",
+                "Chemistry",
+                "Computer and Information Science",
+                "Earth and Environmental Sciences",
+                "Engineering",
+                "Law",
+                "Mathematical Sciences",
+                "Medicine, Health and Life Sciences",
+                "Physics",
+                "Social Sciences",
+                "Other",
+                "Demo Only",
+            ],
+            "description": "Controlled list of subjects for DEMO Dataverse",
+        },
+        "accessRights": {
+            "values": ["open", "restricted", "embargoed", "closed"],
+            "description": "Controlled list of access rights for RDR",
+        },
+        "legitimateOptout": {
+            "values": [
+                "privacy",
+                "intellectual property rights",
+                "ethical aspects",
+                "aspects of dual use",
+                "other",
+            ],
+            "description": "Controlled list of legitimate opt out for RDR",
+        },
+    }
 
-            }
-    
     return controlled_vocabularies[name]["values"]
-    
+
 
 def create_tmp_folder():
     directory_name = "tmp"
@@ -66,10 +58,9 @@ def create_tmp_folder():
     try:
         new_directory_path.mkdir(exist_ok=True)
     except Exception as e:
-        print(f"An error occured: {e}")
-    
-    return new_directory_path.resolve()
+        print(f"An error occurred: {e}")
 
+    return new_directory_path.resolve()
 
 
 def check_typeClass(field):
@@ -83,18 +74,23 @@ def check_typeClass(field):
 
 
 def primitive_field(field):
-    if  re.match(r".*email.*", field["typeName"], re.IGNORECASE):
+    if re.match(r".*email.*", field["typeName"], re.IGNORECASE):
         field["value"] = get_email(field)
-    elif  re.match (r".*date.*", field["typeName"], re.IGNORECASE):
+    elif re.match(r".*date.*", field["typeName"], re.IGNORECASE):
         field["value"] = get_date(field)
     else:
-        field["value"] = Prompt.ask(field['typeName'], default=f"placeholder {field['typeName']}")
-
+        field["value"] = Prompt.ask(
+            field["typeName"], default=f"placeholder {field['typeName']}"
+        )
 
 
 def controlled_vocabulary(field):
     controlled_vocabulary_list = get_controlled_vocabulary(field["typeName"])
-    value = Prompt.ask(f"Choose one {field['typeName']} from the controlled vocabulary (additional values can be added later):", choices=controlled_vocabulary_list, default=controlled_vocabulary_list[-1])
+    value = Prompt.ask(
+        f"Choose one {field['typeName']} from the controlled vocabulary (additional values can be added later):",
+        choices=controlled_vocabulary_list,
+        default=controlled_vocabulary_list[-1],
+    )
     if field["multiple"]:
         field["value"] = [value]
     else:
@@ -106,30 +102,36 @@ def compound_field(field):
         for i in range(len(field["value"])):
             for child_value in field["value"][i].values():
                 # print(child_value)
-                check_typeClass(child_value)       # check child fields recursively
+                check_typeClass(child_value)  # check child fields recursively
     else:
         for child_value in field["value"]:
-            check_typeClass(field["value"][child_value])        # check child fields recursively
-
+            check_typeClass(
+                field["value"][child_value]
+            )  # check child fields recursively
 
 
 def get_email(field):
     email = None
     while not re.match(r"[^@]+@[^@]+\.[^@]+", str(email)):
-        email = Prompt.ask(f"enter a valid {field['typeName']}", default="placeholder@placeholder.com")
+        email = Prompt.ask(
+            f"enter a valid {field['typeName']}", default="placeholder@placeholder.com"
+        )
     return email
+
 
 def get_date(field):
     date = None
     while not re.match(r"\d\d\d\d-\d\d-\d\d", str(date)):
-        date = Prompt.ask(f"enter a valid {field['typeName']} (YYYY-MM-DD)", default=datetime.today().strftime("%Y-%m-%d"))
+        date = Prompt.ask(
+            f"enter a valid {field['typeName']} (YYYY-MM-DD)",
+            default=datetime.today().strftime("%Y-%m-%d"),
+        )
     return date
-
 
 
 def fill_in_md_template(path_to_template):
 
-    with open(path_to_template , "r") as f:
+    with open(path_to_template, "r") as f:
         dataset = json.load(f)
 
     blocks = dataset["datasetVersion"]["metadataBlocks"]
@@ -144,9 +146,3 @@ def fill_in_md_template(path_to_template):
         with open(file_path / "tmp_file.json", "w") as f:
             json.dump(dataset, f)
         return str(file_path / "tmp_file.json")
-
-
-
-        
-
-
