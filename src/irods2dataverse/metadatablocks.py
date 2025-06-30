59354365285a9add843b6097f733a8dc97d18434
joachim <joachim.bovin@kuleuven.be>
2025-06-30 14:21:45 +0200

    Started refactor for metadatablocks.py

--- src/irods2dataverse/metadatablocks.py
+++ src/irods2dataverse/metadatablocks.py
@@ -3,12 +3,10 @@ from pyDataverse.api import NativeApi
 
 
 class Metadatablocks(object):
-    """
-    class to request metadatablocks from dv installation, clean response, create uploadable template, prompt users for
-    input & validate using jsonschema
+    """Class to request metadatablocks from dv installation and create template.
     """
 
-    def __init__(self, dv_installation, dv_api_key, extra_fields=None):
+    def __init__(self, dv_installation, dv_api_key, extra_fields=[]):
         self.dv_installation = dv_installation
         self.dv_api_key = dv_api_key
         self.dv_url = ""
@@ -21,7 +19,6 @@ class Metadatablocks(object):
             "typeName": "name of the field",
         }
         self.extra_fields = extra_fields
-        self.check_extra_fields()
         self.basic_blocks = [  # these are the blocks that you want to include for now we only use citation
             #  "geospatial",
             #  "socialscience",
@@ -31,13 +28,10 @@ class Metadatablocks(object):
             "citation",
             #  "computationalworkflow",
         ]
-        self.controlled_vocabularies = {}
+        self.all_controlled_vocabularies = {}
         self.schema = ""
 
-    ########## functions to dynamically get & clean metadatablocks ##############
-    ####  the API returns a number of blocks and each block contains a number of fields = metadata
-
-    def set_dv_url(self):
+    def _set_dv_url(self):
         match self.dv_installation.lower():
             case "rdr-pilot":
                 self.dv_url = "https://www.rdm.libis.kuleuven.be/"
@@ -56,23 +50,10 @@ class Metadatablocks(object):
                     "this dataverse is not configured: the following installations are available: Demo, RDR, RDR-pilot"
                 )
 
-    def check_extra_fields(self):
-        """
-        This functions checks which extra fields should be added to the metadata document specified by the users
-        these are fields that are not listed as required in the template but can be added by the users
-        """
-        if self.extra_fields == None:
-            self.extra_fields = []
-        else:
-            pass
 
-    def get_mdblocks(self):
-        """
-        gets metadatablocks from dataverse
-
-        """
-        self.set_dv_url()
-        print(self.dv_url)
+    def _set_mdblocks(self):
+        """Sets instance variable mdblocks."""
+        self._set_dv_url()
         api = NativeApi(self.dv_url, self.dv_api_key)
         mdblocks_overview = api.get_metadatablocks().json()
         self.mdblocks = {}
@@ -80,16 +61,9 @@ class Metadatablocks(object):
             self.mdblock = api.get_metadatablock(block["name"]).json()
             self.mdblocks[block["name"]] = self.mdblock["data"]
 
-    def remove_childfields(self):
-        """
-        removes the fields from the top level that already exist as childfields of a compound field
-
-        Parameters:
-        ----------
-        block: block name (string)
-
-        """
-        for block in [k for k in self.mdblocks]:
+    def _remove_childfields(self):
+        """Removes the fields from the top level that already exists as childfields of a compound field"""
+        for block in self.mdblocks:
             compound_fields = {
                 k: v
                 for k, v in self.mdblocks[block]["fields"].items()
@@ -97,7 +71,7 @@ class Metadatablocks(object):
             }  # get all the compound fields
             double_fields = {}
             for key in compound_fields:  # make a list with all the child fields
-                double_fields[key] = [k for k in compound_fields[key]["childFields"]]
+                double_fields[key] = list(compound_fields[key]["childFields"])
             for field in double_fields:
                 for child_field in double_fields[field]:
                     del self.mdblocks[block]["fields"][
@@ -105,58 +79,39 @@ class Metadatablocks(object):
                     ]  # delete the child fields from the top level
 
     def write_clean_mdblocks(self):
-        """
-        gets metadatablocks from api, cleans them & write to file
-
-        """
-        self.get_mdblocks()
-        self.remove_childfields()
+        """Gets metadatablocks from api, cleans them & writes to a json document."""
+        self._set_mdblocks()
+        self._remove_childfields()
         with open(f"{self.dv_installation}_metadatablocks_full.json", "w") as f:
             json.dump(self.mdblocks, f)
 
-    def clean_mdblocks(self):
-        """
-        gets metadatablocks from api and cleans them
-        """
-        self.get_mdblocks()
-        self.remove_childfields()
+    def _clean_mdblocks(self):
+        """Gets metadatablocks from api and cleans them."""
+        self._set_mdblocks()
+        self._remove_childfields()
 
-    ###### get all the controlled vocabularies ###############
 
-    def get_controlled_vocabularies(self):
-        """
-        This function gets all the controlled vocabularies
+    def set_all_controlled_vocabularies(self):
+        """ This function gets all the controlled vocabularies
         """
         if not self.mdblocks:  # create md_blocks if empty
-            self.clean_mdblocks()
+            self._clean_mdblocks()
         for k, v in self.mdblocks["citation"]["fields"].items():
             if v["isControlledVocabulary"]:
-                self.controlled_vocabularies[k] = v["controlledVocabularyValues"]
+                self.all_controlled_vocabularies[k] = v["controlledVocabularyValues"]
             if "childFields" in k:
                 for ck, cv in k["childFields"].items():
                     if cv["isControlledVocabulary"]:
-                        self.controlled_vocabularies[ck] = cv[
+                        self.all_controlled_vocabularies[ck] = cv[
                             "controlledVocabularyValues"
                         ]
-        # print(self.controlled_vocabularies)
+        # print(self.all_controlled_vocabularies)
 
-    ########## create templates ##############
 
-    def create_field(self, value, typeClass, compound=None):
-        """
-        This function makes a copy of the template (field_info) and fills in the necessary
-        information based on the provided parameters: either compound or not compound
-        Parameters:
-        ---------
-        field_info : dictionary
-        value: dictionary
-        typeClass = string
-        compound: optional parameter
-
-        Returns:
-        --------
-        field
-
+    def create_field(self, value: dict, typeClass: str, compound=None) -> dict:
+        """ This function makes a copy of the template (field_info) and fills in
+        the necessary information based on the provided parameters: either 
+        compound or not compound.
         """
         new_field = self.field_template.copy()
         if compound is not None:
@@ -166,7 +121,7 @@ class Metadatablocks(object):
         new_field["typeName"] = value["name"]
         return new_field
 
-    def add_child(self, cv, ck):
+    def add_child(self, child_value: dict, child_key: str):
         """
         add childfields and return dictionary
 
@@ -181,21 +136,19 @@ class Metadatablocks(object):
         filled in field template or false
 
         """
-        if cv["isRequired"] or ck in self.extra_fields:
-            if cv["typeClass"] == "primitive":
-                new_field = self.create_field(cv, "primitive")
+        if child_value["isRequired"] or child_key in self.extra_fields:
+            if child_value["typeClass"] == "primitive":
+                new_field = self.create_field(child_value, "primitive")
                 return new_field
-            elif cv["typeClass"] == "controlledVocabulary":
-                new_field = self.create_field(cv, "controlledVocabulary")
+            elif child_value["typeClass"] == "controlledVocabulary":
+                new_field = self.create_field(child_value, "controlledVocabulary")
                 return new_field
             else:
                 return False
 
-    def add_required(self, all_blocks, block):
+    def add_required(self, all_blocks: dict, block: str):
+        """Function to add required fields, goes through all the blocks (citation, ...) and checks if required
         """
-        function to add required fields, goes through all the blocks (citation, ...) and checks if required
-        """
-        # field_info_template = get_field_info_template()
         all_fields = []
         for k, v in all_blocks[block]["fields"].items():
             if v["isRequired"] or k in self.extra_fields:  # check if required
@@ -225,12 +178,8 @@ class Metadatablocks(object):
         return all_fields
 
     def create_json_to_upload(self):
-        """
-        This function creates & writes the json
-
-        """
-        self.clean_mdblocks()
-        # do multiple blocks go in a list?! --> negative
+        """This function creates & writes the json"""
+        self._clean_mdblocks()
         block_dict = {}
         for block in self.basic_blocks:
             try:
@@ -248,16 +197,15 @@ class Metadatablocks(object):
         with open(f"{self.dv_installation}_md.json", "w") as f:
             json.dump(total_template, f)
 
-    def find_controlled_vocabulary(self, name):
+    def find_controlled_vocabulary(self, name: str) -> dict:
+        """ This method takes the typeName of a field and
+        returns a list of the possible values for the controlled vocabulary
         """
-        This method takes the typeName of a field and returns a list of the possible
-        values for the controlled vocabulary
-        """
-        if not self.controlled_vocabularies:
-            self.get_controlled_vocabularies()
+        if not self.all_controlled_vocabularies:
+            self.get_all_controlled_vocabularies()
 
-        if name in self.controlled_vocabularies:
-            controlled_vocabulary = self.controlled_vocabularies[name]
+        if name in self.all_controlled_vocabularies:
+            controlled_vocabulary = self.all_controlled_vocabularies[name]
         return controlled_vocabulary
 
 
@@ -278,5 +226,5 @@ if __name__ == "__main__":
         ],
     )
     blocks.create_json_to_upload()
-    # blocks.get_controlled_vocabularies()
+    # blocks.get_all_controlled_vocabularies()
     #  blocks.write_clean_mdblocks()
